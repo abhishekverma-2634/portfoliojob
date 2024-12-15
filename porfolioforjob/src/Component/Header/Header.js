67412a0b6178df29738b600b116c8472ecd9b544
abhishekverma942 <125015488+abhishekverma-2634@users.noreply.github.com>
2024-12-16 01:37:49 +0530

    header responsive

--- porfolioforjob/src/Component/Header/Header.js
+++ porfolioforjob/src/Component/Header/Header.js
@@ -1,13 +1,20 @@
 import React from "react";
 import "./Header.css";
+import {useState} from "react";
 function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  }
   return (
-    <header className="styleHeader">
+    <header className="header">
+      <div className="styleHeader">
       <div className="logoName">
         <img src="../download.jpg"/>
         <p>Abhishek Verma</p>
       </div>
-      <nav className="nav">
+      <button className="hamburgerMenu" onClick={toggleMenu}>&#9776;</button>
+      <nav className={`nav ${isMenuOpen ? "open" : ""}`}>
         <li> <a href="#home">Home</a></li>
         <li> <a href="#about">About</a></li>
         <li> <a href="#services">Services</a></li>
@@ -15,6 +22,7 @@ function Header() {
 
 
       </nav>
+      </div>
     </header>
   );
 }

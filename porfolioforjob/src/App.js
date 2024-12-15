4009c82d2b3955a265a050eb4fd9febd914f7044
abhishekverma942 <125015488+abhishekverma-2634@users.noreply.github.com>
2024-12-16 02:15:29 +0530

    deploy test

--- porfolioforjob/src/App.js
+++ porfolioforjob/src/App.js
@@ -1,13 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
 import Header from './Component/Header/Header';
 
 function App() {
   return (
     <div>
     <Header />
-    {/* <Body/> */}
-    {/* <Footer /> */}
     </div>
   );
 }

e9169e2bb3b1d00df2992cf4b54d42531b075883
abhishekverma942 <125015488+abhishekverma-2634@users.noreply.github.com>
2024-12-17 14:45:46 +0530

    responsive design

--- porfolioforjob/src/Component/Header/Header.js
+++ porfolioforjob/src/Component/Header/Header.js
@@ -9,11 +9,11 @@ function Header() {
   return (
     <header className="header">
       <div className="styleHeader">
-      <div className="logoName">
+      <div className={`logoName ${isMenuOpen ? "hideLogo" : ""}`}>
         <img src="../download.jpg"/>
         <p>Abhishek Verma</p>
       </div>
-      <button className="hamburgerMenu" onClick={toggleMenu}>&#9776;</button>
+      <button className="hamburgerMenu" onClick={toggleMenu}>{isMenuOpen ? "\u2716" : "\u2630"}</button>
       <nav className={`nav ${isMenuOpen ? "open" : ""}`}>
         <li> <a href="#home">Home</a></li>
         <li> <a href="#about">About</a></li>
